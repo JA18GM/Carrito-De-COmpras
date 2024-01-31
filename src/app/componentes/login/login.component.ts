0a49ee91b50e97e7f11ca3cc6e4eb0a1a548daba
JA18GM <jesusandresgonzalezmanzanales@gmail.com>
2024-01-31 11:19:41 -0600

    LOGIN

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -9,10 +9,14 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class LoginComponent {
   loginForma= this.fb.group({
     email: ['',[Validators.required, Validators.email]],
-    password: ['',[Validators.required,]],
+    password: ['',[Validators.required, Validators.minLength(8),
+      Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/)
+    ]],
 
   }
   );
+confirmPassword: any;
+registerForma: any;
 
   constructor(private fb: FormBuilder) {
 
